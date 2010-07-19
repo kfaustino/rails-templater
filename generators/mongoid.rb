82b44939855c2f8a24f95e0a27432359c7293353
Kevin Faustino <kevin.faustino@gmail.com>
2010-07-19 18:10:27 -0400

    Tempalte for mongoid

---
+++ generators/mongoid.rb
@@ -0,0 +1,5 @@
+gem 'mongoid', '>= 2.0.0.beta9'
+gem 'bson_ext', '1.0.4'
+
+run 'bundle install'
+generate 'mongoid:config'
--- template.rb
+++ template.rb
@@ -2,3 +2,4 @@ require File.join(File.dirname(__FILE__), 'core_extensions.rb')
 
 apply generator_path('default')
 apply generator_path('jquery')
+apply generator_path('mongoid')
