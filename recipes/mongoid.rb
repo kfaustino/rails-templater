6f5994c41bffb88fc738f4f81a04ed9d33b155bb
Kevin Faustino <kevin.faustino@gmail.com>
2010-07-21 21:38:13 -0400

    Stub rake task db:test:prepare so mongoid can work with cucumber rake tasks

--- recipes/mongoid.rb
+++ recipes/mongoid.rb
@@ -1,6 +1,8 @@
 gem 'mongoid', '>= 2.0.0.beta9'
 gem 'bson_ext', '1.0.4'
 
+create_file 'lib/tasks/mongo.rake', load_template('lib/tasks/mongo.rake', 'mongoid')
+
 stategies << lambda do
   generate 'mongoid:config'
 end
