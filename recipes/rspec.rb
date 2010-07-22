f0900e1984b983734944efe3f3d9a2ff4cb4ae07
Kevin Faustino <kevin.faustino@gmail.com>
2010-07-21 22:03:03 -0400

    Drop Mongoid collections before each spec

--- recipes/rspec.rb
+++ recipes/rspec.rb
@@ -4,4 +4,24 @@ group :development, :test do
 end
 EOF
 
-stategies << lambda { generate 'rspec:install' }
+stategies << lambda do
+  generate 'rspec:install'
+  
+  spec_helper_path = 'spec/spec_helper.rb'
+  
+  inject_into_file spec_helper_path, "\nrequire 'factory_girl'", :after => "require 'rspec/rails'"
+  
+  gsub_file spec_helper_path, 'config.fixture_path = "#{::Rails.root}/spec/fixtures"', ''
+  gsub_file spec_helper_path, /(config.use_transactional_fixtures = true)/, '# \1'
+  
+  mongoid_rspec_truncation = <<-MONGOID
+  
+    config.before :each do
+      Mongoid.master.collections.select {|c| c.name !~ /system/ }.each(&:drop)
+    end
+    
+  MONGOID
+    
+  inject_into_file spec_helper_path, mongoid_rspec_truncation, :after => "# config.use_transactional_fixtures = true\n"
+
+end
