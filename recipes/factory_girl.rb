cbf67f035d28467fc83344e284692b6a4031aa16
Kevin Faustino <kevin.faustino@gmail.com>
2010-07-21 22:08:15 -0400

    Moved factory_girl RSpec require to factory_girl recipe

--- recipes/factory_girl.rb
+++ recipes/factory_girl.rb
@@ -1,1 +1,5 @@
 gem 'factory_girl_rails', :group => :test
+
+stategies << lambda do
+  inject_into_file 'spec/spec_helper.rb', "\nrequire 'factory_girl'", :after => "require 'rspec/rails'"
+end
--- recipes/rspec.rb
+++ recipes/rspec.rb
@@ -1,16 +1,10 @@
-append_file 'Gemfile', <<-EOF
-group :development, :test do
-  gem 'rspec-rails', '>= 2.0.0.beta.17'
-end
-EOF
+gem 'rspec-rails', '>= 2.0.0.beta.17', :group => [:development, :test]
 
 stategies << lambda do
   generate 'rspec:install'
   
   spec_helper_path = 'spec/spec_helper.rb'
-  
-  inject_into_file spec_helper_path, "\nrequire 'factory_girl'", :after => "require 'rspec/rails'"
-  
+    
   gsub_file spec_helper_path, 'config.fixture_path = "#{::Rails.root}/spec/fixtures"', ''
   gsub_file spec_helper_path, /(config.use_transactional_fixtures = true)/, '# \1'
   
