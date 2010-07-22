6f52cbcfb306e91a853278f1920fbef62279ee26
Kevin Faustino <kevin.faustino@gmail.com>
2010-07-21 21:39:06 -0400

    Restore public/favicon.ico

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,6 @@
 # Delete all unnecessary files
 remove_file "README"
 remove_file "public/index.html"
-remove_file "public/favicon.ico"
 remove_file "public/robots.txt"
 remove_file "public/images/rails.png"
 
