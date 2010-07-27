f41d7baa2edc8d198d24419a834e240814b42295
Kevin Faustino <kevin.faustino@gmail.com>
2010-07-27 18:54:53 -0400

    No longer monkeypatching AssetTagHelper to use custom javascript defaults

--- recipes/jquery.rb
+++ recipes/jquery.rb
@@ -2,6 +2,5 @@
 get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js",  "public/javascripts/jquery.js"
 get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
 
-#initializer "jquery.rb",  File.expand_path('config/initializers/jquery.rb', jquery_template_path)
-#copy_file , 'config/initializers/jquery.rb'
-initializer 'jquery.rb', load_template('config/initializers/jquery.rb','jquery')
+# Set default JavaScript files
+environment "config.action_view.javascript_expansions = { :defaults => ['jquery', 'rails'] }"
