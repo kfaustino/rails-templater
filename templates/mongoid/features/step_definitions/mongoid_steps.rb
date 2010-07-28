1fb6ddd0f869b696d8d89961819303b0ca820238
Kevin Faustino <kevin.faustino@gmail.com>
2010-07-28 16:05:00 -0400

    Update mongoid step definition to match Factory girl naming of model for steps

--- templates/mongoid/features/step_definitions/mongoid_steps.rb
+++ templates/mongoid/features/step_definitions/mongoid_steps.rb
@@ -1,3 +1,4 @@
-Given /^an (.+) exists with an? (.+) of "([^"]*)"$/ do |model, field, value|
-  Factory model, field => value
+Given /^an? (.+) exists with an? (.+) of "([^"]*)"$/ do |model, field, value|
+  factory_name = model.gsub(' ', '_')
+  Factory factory_name, field => value
 end
