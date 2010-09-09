66490a2604a950bd266a796ee6441b6e972b365c
Dominik Masur <dominik.masur@googlemail.com>
2010-09-09 22:53:38 +0200

    using the jquery-rails gem

--- recipes/jquery.rb
+++ recipes/jquery.rb
@@ -1,6 +1,3 @@
-# Initialize jQuery
-get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js",  "public/javascripts/jquery.js"
-get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
+gem 'jquery-rails'
 
-# Set default JavaScript files
-environment "config.action_view.javascript_expansions = { :defaults => ['jquery', 'rails'] }"
+generate 'jquery:install'
