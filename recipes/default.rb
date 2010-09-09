9627b81c3b1b1b8925aabae874a6e5353117d446
Dominik Masur <dominik.masur@googlemail.com>
2010-09-09 23:29:54 +0200

    added rvm for 1.8.7

--- recipes/default.rb
+++ recipes/default.rb
@@ -8,6 +8,10 @@ create_file 'README'
 create_file 'log/.gitkeep'
 create_file 'tmp/.gitkeep'
 
+# .rvmrc
+create_file '.rvmrc', 'rvm 1.8.7'
+run 'rvm rvmrc trust'
+
 gsub_file 'config/application.rb', 'require "rails/test_unit/railtie"', '# require "rails/test_unit/railtie"'
 
 get "http://html5shiv.googlecode.com/svn/trunk/html5.js", "public/javascripts/html5.js"
