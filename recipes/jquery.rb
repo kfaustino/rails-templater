b4260486621912c056e44ea790f32d8516c0f3e4
Kevin Faustino <kevin.faustino@gmail.com>
2010-10-06 12:40:05 -0400

    [FIX] Ensure javascript_expansions are set before initialization

--- recipes/jquery.rb
+++ recipes/jquery.rb
@@ -3,4 +3,10 @@ get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js",  "public/
 get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
 
 # Set default JavaScript files
-environment "config.action_view.javascript_expansions = { :defaults => ['jquery', 'rails'] }"
+jquery_configuration = <<-END
+config.before_initialize do
+  config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+end
+END
+
+environment jquery_configuration
