bae98a507fe2e534d32dd4d007ad3e792207b8db
Kevin Faustino <kevin.faustino@gmail.com>
2010-11-04 19:33:32 -0400

    [FIX] cucumber recipe

--- recipes/cucumber.rb
+++ recipes/cucumber.rb
@@ -1,17 +1,16 @@
 gem 'capybara', :group => :test
 gem 'cucumber-rails', :group => :test
-gem 'launchy', :group => :launchy
+gem 'launchy', :group => :test
 
 stategies <<  lambda do
   generate 'cucumber:install --rspec --capybara --skip-database'
 
   cukes_factory_girl = <<-END
-
   require 'factory_girl'
   require 'factory_girl/step_definitions'
   Dir[File.expand_path(File.join(File.dirname(__FILE__),'..','..','spec','factories','*.rb'))].each {|f| require f}
-
   END
+
   inject_into_file "features/support/env.rb", cukes_factory_girl, :after => 'ActionController::Base.allow_rescue = false'
 
   # Mongoid truncation strategy
