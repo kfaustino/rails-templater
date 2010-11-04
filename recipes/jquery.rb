e01420ca378e3de28bf926df4193ad6f2479e563
Kevin Faustino <kevin.faustino@gmail.com>
2010-11-04 09:43:08 -0400

    [Issue #3] Load rails.js as a template until Thor accepts https sources on a get

--- recipes/jquery.rb
+++ recipes/jquery.rb
@@ -1,11 +1,11 @@
 # Initialize jQuery
-get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js",  "public/javascripts/jquery.js"
-get "http://github.com/rails/jquery-ujs/raw/master/src/rails.js", "public/javascripts/rails.js"
+get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js",  "public/javascripts/jquery.js"
+create_file 'public/javascripts/rails.js', load_template('rails.js', 'jquery')
 
 # Set default JavaScript files
 jquery_configuration = <<-END
 config.before_initialize do
-  config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+  config.action_view.javascript_expansions[:defaults] = %w(jquery rails application)
 end
 END
 
