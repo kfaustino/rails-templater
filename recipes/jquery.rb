bc1c85ad3a595095f72a1f5c4851b05975591d42
Kevin Faustino <kevin.faustino@gmail.com>
2010-11-04 19:59:21 -0400

    Utilize jquery-rails for jQuery recipe

--- recipes/jquery.rb
+++ recipes/jquery.rb
@@ -1,12 +1,1 @@
-# Initialize jQuery
-get "http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js",  "public/javascripts/jquery.js"
-create_file 'public/javascripts/rails.js', load_template('rails.js', 'jquery')
-
-# Set default JavaScript files
-jquery_configuration = <<-END
-config.before_initialize do
-  config.action_view.javascript_expansions[:defaults] = %w(jquery rails application)
-end
-END
-
-environment jquery_configuration
+gem 'jquery-rails'
