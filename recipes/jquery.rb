e20d84dd14e5b2da5eee948d71440996cd275c98
Kevin Faustino <kevin.faustino@gmail.com>
2010-11-04 20:51:34 -0400

    Comment out config.action_view.javascript_expansions in application.rb for jquery-rails

--- recipes/jquery.rb
+++ recipes/jquery.rb
@@ -1,5 +1,7 @@
 gem 'jquery-rails'
 
+gsub_file 'config/application.rb', /(config.action_view.javascript_expansions\[:defaults\] = %w\(\))/, '# \1'
+
 stategies << lambda do
   generate 'jquery:install'
 end
