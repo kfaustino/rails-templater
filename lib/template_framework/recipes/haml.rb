a0b6f381326a674788e69c6e5e6de41a3dd9b80d
Kevin Faustino <kevin.faustino@gmail.com>
2011-01-29 16:24:12 -0500

    Ask user if they would like Haml for their Template Engine

--- lib/template_framework/recipes/haml.rb
+++ lib/template_framework/recipes/haml.rb
@@ -1,5 +1,7 @@
-gem 'haml'
-gem "haml-rails"
+if yes?("\n[Template Engine] Would you like use Haml for your view templates? [y|n]: ", Thor::Shell::Color::BLUE)
+  gem 'haml'
+  gem "haml-rails"
 
-remove_file 'app/views/layouts/application.html.erb'
-create_file 'app/views/layouts/application.html.haml', templater.load_template('app/views/layouts/application.html.haml','haml')
+  remove_file 'app/views/layouts/application.html.erb'
+  create_file 'app/views/layouts/application.html.haml', templater.load_template('app/views/layouts/application.html.haml','haml')
+end
