49f874ac7bb0fb50c06268e281babe0f56b7e946
Kevin Faustino <kevin.faustino@gmail.com>
2011-02-06 12:48:50 -0500

    Update Mongoid recipe to version 2.0.0.rc.7

--- lib/template_framework/recipes/mongoid.rb
+++ lib/template_framework/recipes/mongoid.rb
@@ -1,6 +1,6 @@
 say("\nReplacing ActiveRecord with Mongoid\n", Thor::Shell::Color::YELLOW )
 
-gem 'mongoid', '2.0.0.rc.6'
+gem 'mongoid', '2.0.0.rc.7'
 gem 'bson_ext', '~> 1.2'
 
 gsub_file 'config/application.rb', 'require "active_record/railtie"', '# require "active_record/railtie"'
