270a2481a0b0ef09f5f4368ffcd901d8e5702c7f
lhoeg <larshoeg@gmail.com>
2011-03-03 17:42:23 +0100

    Move .gitkeep file creation to git recipe

--- lib/template_framework/recipes/default.rb
+++ lib/template_framework/recipes/default.rb
@@ -6,8 +6,6 @@ remove_file "public/images/rails.png"
 
 # Create project files
 create_file 'README'
-create_file 'log/.gitkeep'
-create_file 'tmp/.gitkeep'
 
 get "http://html5shiv.googlecode.com/svn/trunk/html5.js", "public/javascripts/html5.js"
 
--- lib/template_framework/recipes/git.rb
+++ lib/template_framework/recipes/git.rb
@@ -1,3 +1,5 @@
 git :init
 
+create_file 'log/.gitkeep'
+create_file 'tmp/.gitkeep'
 append_file '.gitignore', templater.load_template('gitignore','git')
