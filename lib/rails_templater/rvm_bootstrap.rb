e55937f9394734fd3010c76be9050316c8a0f461
lhoeg <larshoeg@gmail.com>
2011-03-15 09:43:25 +0100

    Ask if we should use RVM

--- lib/rails_templater/rvm_bootstrap.rb
+++ lib/rails_templater/rvm_bootstrap.rb
@@ -1,18 +1,36 @@
+require 'thor/shell'
+
 module RailsTemplater
   class RvmBootstrap
 
-    def self.init(gemset_name)
+    def self.init(application_name)
       return unless require_rvm!
 
-      puts "Initializing rvm gemset #{current_ruby}@#{gemset_name}"
+      thor = Thor::Shell::Basic.new
+      return unless thor.yes?("\nWould you like to use RVM? [y|n]: ", Thor::Shell::Color::BLUE)
+
+      thor.say "\nWhich Ruby would you like to use?\n", Thor::Shell::Color::BLUE
+      ruby_options = { 'Option' => 'Ruby' }
+      installed_rubies.each_with_index do |ruby, idx|
+        ruby_options[idx+1] = ruby + "#{' [default]' if ruby == current_ruby}"
+      end
+      thor.print_table ruby_options.to_a, :ident => 4
+      ruby_selection = thor.ask("\nOption: ", Thor::Shell::Color::BLUE)
+      ruby_selection = ruby_selection.blank?? current_ruby : installed_rubies[ruby_selection.to_i-1]
+
+      thor.say "\nWhich Gemset would you like to use?\n", Thor::Shell::Color::BLUE
+      gemset_selection = thor.ask("\nGemset [default: #{application_name}]: ", Thor::Shell::Color::BLUE)
+      gemset_selection = application_name if gemset_selection.empty?
+
+      thor.say "Initializing rvm gemset #{ruby_selection}@#{gemset_selection}"
       env = RVM::Environment.new(current_ruby)
-      env.gemset_create(gemset_name)
-      env.gemset_use!(gemset_name)
+      env.gemset_create(gemset_selection)
+      env.gemset_use!(gemset_selection)
 
       %w(bundler rails).each do |gem_name|
         unless installed_gems.include? gem_name
-          puts "Installing #{gem_name} gem..."
-          puts "Successfully installed #{gem_name}" if system("gem", "install", gem_name)
+          thor.say "Installing #{gem_name} gem..."
+          thor.say "Successfully installed #{gem_name}" if system("gem", "install", gem_name)
         end
       end
     end
@@ -31,6 +49,10 @@ module RailsTemplater
 
     def self.installed_gems
       `gem list`.split("\n").map { |gem| /([^ ]+)/ =~ gem; $1 }
+    end
+
+    def self.installed_rubies
+      rubies = RVM::Environment.current.list.rubies
     end
 
     def self.require_rvm!
