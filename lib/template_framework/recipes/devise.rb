7004b25d90ddcaab7342bb4ad91820e0e05d4241
lhoeg <larshoeg@gmail.com>
2011-04-01 10:01:03 +0200

    Added option to change devise model name and optionally generate views (thanks to morcmarc)

--- lib/template_framework/recipes/devise.rb
+++ lib/template_framework/recipes/devise.rb
@@ -1,6 +1,12 @@
 gem 'devise'
 
+model_name = ask('Name of Devise model? [user]: ', Thor::Shell::Color::BLUE)
+model_name = 'user' if model_name.blank?
+
+generate_views = yes?('Generate Devise views? [y|n]: ', Thor::Shell::Color::BLUE)
+
 templater.post_bundler do
   generate "devise:install"
-  generate "devise user"
+  generate "devise #{model_name}"
+  generate "devise:views -e #{templater.template_engine.type}" if generate_views
 end
