650591d52bdea790ca1ff3096d23684fd551b95a
lhoeg <larshoeg@gmail.com>
2011-04-01 13:52:49 +0200

    Added devise view conversion to haml/slim (haml and slim support was recently removed from devise gem)

--- lib/template_framework/recipes/devise.rb
+++ lib/template_framework/recipes/devise.rb
@@ -5,10 +5,29 @@ model_name = 'user' if model_name.blank?
 
 generate_views = yes?('Generate Devise views? [y|n]: ', Thor::Shell::Color::BLUE)
 
+if generate_views && templater.template_engine.slim?
+  # hpricot and haml2slim is required for converting to slim templates
+  gem 'haml2slim', :group => :development
+  gem 'hpricot'
+end
+
 templater.post_bundler do
   generate "devise:install"
   generate "devise #{model_name}"
-  generate "devise:views -e #{templater.template_engine.type}" if generate_views
+  generate "devise:views -e erb" if generate_views
+  if generate_views && (templater.template_engine.haml? || templater.template_engine.slim?)
+    erb_files = Dir.glob File.join("app", "views", "devise", "*", "*.erb")
+    erb_files.each do |erb_file|
+      haml_file = erb_file.sub /erb$/, 'haml'
+      system "bundle exec html2haml --erb #{erb_file} #{haml_file}"
+      remove_file erb_file
+
+      if templater.template_engine.slim?
+        system "bundle exec haml2slim #{haml_file} #{erb_file.sub /erb$/, 'slim'}"
+        remove_file haml_file
+      end
+    end
+  end
 end
 
 # Modify ActionMailer configuration for Devise
