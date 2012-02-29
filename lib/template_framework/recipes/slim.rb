03e920798a806ae10619a9c4e7bb028fbd1166ea
Kevin Faustino <kevin.faustino@gmail.com>
2012-02-29 16:44:47 -0500

    Updating slim recipe to use slim-rails

--- lib/template_framework/recipes/slim.rb
+++ lib/template_framework/recipes/slim.rb
@@ -1,6 +1,6 @@
 say "\nReplacing ERb with Slim as your Template Engine\n", Thor::Shell::Color::YELLOW
 
-gem 'slim', :require => 'slim/rails'
+gem 'slim-rails'
 
 remove_file 'app/views/layouts/application.html.erb'
 create_file 'app/views/layouts/application.html.slim', templater.load_template('app/views/layouts/application.html.slim', 'slim')
