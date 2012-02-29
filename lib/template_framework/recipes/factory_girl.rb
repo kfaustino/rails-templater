c854a5f50aeeba66ed2b16dd678dad1e63f80920
Kevin Faustino <kevin.faustino@gmail.com>
2012-02-29 17:52:33 -0500

    Updating factory_girl_rails to version 1.2

--- lib/template_framework/recipes/factory_girl.rb
+++ lib/template_framework/recipes/factory_girl.rb
@@ -1,11 +1,11 @@
 if templater.testing_framework.rspec?
   say("\nReplacing Fixtures with FactoryGirl\n", Thor::Shell::Color::YELLOW)
-  gem 'factory_girl_rails', '1.1.beta1', :group => :test
+  gem 'factory_girl_rails', '~> 1.2', :group => :test
 
   templater.post_bundler do
     inject_into_file 'spec/spec_helper.rb', "\nrequire 'factory_girl'", :after => "require 'rspec/rails'"
-    
+
     environment templater.load_snippet('generator', 'factory_girl')
-    directory File.expand_path('./../../generators', __FILE__), 'lib/generators'    
+    directory File.expand_path('./../../generators', __FILE__), 'lib/generators'
   end
 end
